1a96a6863351e69237891403bb61e979b1082baa
juni998 <wodnrdlgla@gmail.com>
2023-01-17 18:33:01 +0900

    fix : real 인식 안되는 문제 - 1 profile.sh IDLE 오타 수정

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -15,20 +15,20 @@ function find_idle_profile() {
 
     if [ ${CURRENT_PROFILE} == real1 ]
     then
-      IDEL_PROFILE=real2
+      IDLE_PROFILE=real2
     else
-      IDEL_PROFILE=real1
+      IDLE_PROFILE=real1
     fi
 
-    echo "${IDEL_PROFILE}"
+    echo "${IDLE_PROFILE}"
 
 }
 
 # 쉬고 있는 profile의 port 찾기
 function find_idle_port() {
-    IDEL_PROFILE=$(find_idle_profile)
+    IDLE_PROFILE=$(find_idle_profile)
 
-    if [ ${IDEL_PROFILE} == real1 ]
+    if [ ${IDLE_PROFILE} == real1 ]
     then
       echo "8081"
     else
